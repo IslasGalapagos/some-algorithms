53935a593fcefcd5a2a6b8a19e972c28212cb39c
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2018-08-10 21:13:34 +0300

    add binary search

---
+++ 01.binary_search.js
@@ -0,0 +1,23 @@
+// ⏳ O(log n)
+
+const binSearch = (list, item) => {
+  let low = 0;
+  let high = list.length - 1;
+
+  while (low <= high) {
+    let mid = Math.ceil((low + high) / 2);
+    let guess = list[mid];
+
+    if (guess === item) {
+      return mid;
+    } else if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return null;
+}
+
+console.log(binSearch([1, 3, 5, 7, 9], 3)) // → 1
